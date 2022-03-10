c090412ba1da2987788e0007151995fb94d9cb50
Cloud <cloud@Clouds-MacBook-Pro.local>
2022-03-10 17:11:30 +0800

    t

--- parts/head.php
+++
@@ -1,5 +0,0 @@
-<meta name="keywords" content="关键词1, 关键词2">
-<meta name="description" content="SEO描述">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="referrer" content="always">
-<link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon">
